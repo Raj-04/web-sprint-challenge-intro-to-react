7413f1e4a5de752507c09b79a986b2813b309ddc
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-13 17:09:17 -0400

    finsihed all work

--- src/components/CharacterInfo.js
+++ src/components/CharacterInfo.js
@@ -1,6 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Style = styled.div`
+font-family: "Times New Roman";
+text-align: center;
+width: 30%;
+height: 20%;
+border: none;
+border-radius: 2px;
+color: white;
+margin: 2%;
+&:hover {
+color: blue; }
+`
+
 const StyledCharacter = styled.div`
 color: white;
 display: block;
@@ -14,22 +27,24 @@ border: 3px solid orange;
 padding: 10px;
 font-weight: bold;
 font-size: 20px;
-background-color: brown;
+background-color: black;
 `
+
+
 
 export default function CharacterInfo(props) {
     const { characterInfo, close } = props
 
     return (
         <StyledCharacter>
-            <p>{characterInfo.name}</p>
-            <p>Gender: {characterInfo.gender}</p>
-            <p>Height: {characterInfo.height}</p>
-            <p>Mass: {characterInfo.mass}</p>
-            <p>BirthYear: {characterInfo.birth_year}</p>
-            <p>Eye Color: {characterInfo.eye_color}</p>
-            <p>Hair Color{characterInfo.hair_color}</p>
-            <p>Skin Color: {characterInfo.skin_color}</p>
+            <Style>{characterInfo.name}</Style>
+            <Style>Gender: {characterInfo.gender}</Style>
+            <Style>Height: {characterInfo.height}</Style>
+            <Style>Mass: {characterInfo.mass}</Style>
+            <Style>BirthYear: {characterInfo.birth_year}</Style>
+            <Style>Eye Color: {characterInfo.eye_color}</Style>
+            <Style>Hair Color{characterInfo.hair_color}</Style>
+            <Style>Skin Color: {characterInfo.skin_color}</Style>
             <button onClick={close}>Close</button>
         </StyledCharacter>
     )
