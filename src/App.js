30d6e271c99ba7a3ea035c0efb1b5ba9635f24bc
Raj Patel <rajpatel@Rajs-MacBook-Pro.local>
2021-08-13 13:53:21 -0400

    State properties and fetch characters from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [characters, setCharacters] = useState([])
+  const [characterDetails, setCharacterDetails] = useState(null)
+  const [error, setError] = useState(null);
+
+  const openInfo = character => {
+    setCharacterDetails( character )
+  }
+
+  const closeInfo = () => {
+    setCharacterDetails(null)
+  }
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+    .then(response => {
+      setCharacters(response.data)
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.error(err);
+      setError("Sorry, Error for the Characters!")
+    })
+  }, [])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -11,7 +35,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT STAR WARS</h1>
     </div>
   );
 }
